569e514b278d6abe0b28742cf51736fa60106b07
Cmanchik <aksenova1648@gmail.com>
2025-10-02 18:27:20 +0400

    Вертикальное движение крана

---
+++ Assets/Scripts/Crane/MovingMechanism/Crane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Crane.MovingMechanism
+{
+    public class Crane : MonoBehaviour
+    {
+        [SerializeField]
+        private LerpMovement m_beam;
+
+        [SerializeField]
+        private LerpMovement m_winch;
+
+        public void ChangeMovementDirection(Vector3 direction)
+        {
+            m_winch.ChangeMovementDirection(direction.x);
+            m_beam.ChangeMovementDirection(direction.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Crane/MovingMechanism/LerpMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Crane.MovingMechanism
+{
+    public class LerpMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform m_positvePoint;
+
+        [SerializeField] 
+        private Transform m_negativePoint;
+
+        [Space]
+
+        [SerializeField]
+        private float m_positiveSpeed;
+
+        [SerializeField]
+        private float m_negativeSpeed;
+
+
+        private Transform m_currentTarget;
+        private float m_currentSpeed;
+
+        private Transform m_transform;
+
+        private void Awake()
+        {
+            m_transform = transform;
+        }
+
+        public void ChangeMovementDirection(float direction)
+        {
+            if (direction > 1)
+            {
+                m_currentTarget = m_positvePoint;
+                m_currentSpeed = m_positiveSpeed;
+            }
+            else if (direction < 0)
+            {
+                m_currentTarget = m_negativePoint;
+                m_currentSpeed = m_negativeSpeed;
+            }
+            else
+            {
+                m_currentTarget = null;
+                m_currentSpeed = 0;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (m_currentTarget != null)
+            {
+                m_transform.position = Vector3.MoveTowards(m_transform.position, m_currentTarget.position, m_currentSpeed);
+            }
+        }
+    }
+}
