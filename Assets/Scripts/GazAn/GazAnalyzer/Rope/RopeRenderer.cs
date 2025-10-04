1ac7c880e62e5347694e88ef7459ee3038848c0f
Cmanchik <aksenova1648@gmail.com>
2025-10-04 12:20:40 +0400

    Создание веревки от газана до зонда

---
+++ Assets/Scripts/GazAn/GazAnalyzer/Rope/RopeRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.GazAn.GazAnalyzer.Rope
+{
+    public class RopeRenderer : MonoBehaviour
+    {
+        [SerializeField]
+        private LineRenderer m_lineRenderer;
+
+        [SerializeField]
+        private Transform[] m_ropeSegments;
+
+        private void Start()
+        {
+            m_lineRenderer.positionCount = m_ropeSegments.Length;
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < m_lineRenderer.positionCount; i++)
+            {
+                m_lineRenderer.SetPosition(i, m_ropeSegments[i].position);
+            }
+        }
+    }
+}
